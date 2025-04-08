4544410021731fb5778fd1e40971af842effffae
Rodrigo LIma <rdrigo019@gmail.com>
2025-04-08 00:06:52 -0300

    modifications

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const API_KEY = 'AIzaSyDsWlWYIgCWc7SN4SZkJIhswcmWXN-l94M'; // Substitua pela sua chave
-const CHANNEL_ID = 'UCOfeXQqErsQbDxassRb7rjg'; // Substitua pelo ID do seu canal
-
 const likesElement = document.getElementById('likes');
 const metaElement = document.getElementById('meta');
 const progressBar = document.getElementById('progress-bar');
@@ -9,23 +7,16 @@ let meta = 50;
 let likes = 0;
 const incremento = 50;
 
-async function getLiveVideoId() {
-  const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&eventType=live&type=video&key=${API_KEY}`
-  );
-  const data = await res.json();
-  if (data.items && data.items.length > 0) {
-    return data.items[0].id.videoId;
-  } else {
-    return null;
-  }
+function getVideoIdFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("videoId");
 }
 
 async function fetchLikes() {
   try {
-    const videoId = await getLiveVideoId();
+    const videoId = getVideoIdFromURL();
     if (!videoId) {
-      likesElement.textContent = 'Live não encontrada';
+      likesElement.textContent = 'ID inválido';
       metaElement.textContent = '';
       return;
     }
@@ -39,8 +30,6 @@ async function fetchLikes() {
 
     if (likes >= meta) {
       meta += incremento;
-
-      // Animações de comemoração
       likesElement.classList.add('pop-animation');
       metaElement.classList.add('pulse-animation');
 
@@ -71,8 +60,5 @@ function updateProgress() {
   }
 }
 
-// Atualiza likes a cada 5 segundos
-setInterval(fetchLikes, 30000);
-
-// Chamada inicial
+setInterval(fetchLikes, 5000);
 fetchLikes();
